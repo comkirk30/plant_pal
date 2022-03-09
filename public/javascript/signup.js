6c7cb13b0c50f1f441b50d1ee536385a5a4c76ef
ThomBurt <93936296+ThomBurt@users.noreply.github.com>
2022-03-08 20:57:27 -0500

    login and signup.js

---
+++ public/javascript/signup.js
@@ -0,0 +1,33 @@
+async function signupFormHandler(event) {
+    event.preventDefault();
+
+// username & email & password & Zip code
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+    const zip = document.querySelector('#zip-signup').value.trim();
+
+    if (username && email && password && zip) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({
+                username,
+                email,
+                password,
+                zip
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        if (response.ok) {
+            console.log(`Success ${username}, you have logged in!`);
+
+
+            document.location.replace('/dashboard');
+
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('#signup-form').addEventListener('submit', signupFormHandler);
