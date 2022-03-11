4444201cf0100a4227402e1688440a4b33a613ae
Emily <emilyadarr@gmail.com>
2022-03-10 21:14:15 -0500

    server update

--- server.js
+++ server.js
@@ -2,38 +2,38 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
-const helpers = require('./utils/helpers');
+//const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // set up Handlebars.js as app's template engine of choice
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers });
+// const exphbs = require('express-handlebars');
+// const hbs = exphbs.create({ helpers });
 
 //sets up an Express.js session and connects the session to our Sequelize Database
-const session = require('express-session');
+// const session = require('express-session');
 
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const sess = {
-  secret: process.env.SESS_SECRET,
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
+// const sess = {
+//   secret: process.env.SESS_SECRET,
+//   cookie: {},
+//   resave: false,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize
+//   })
+// };
 
-app.use(session(sess));
+// app.use(session(sess));
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
