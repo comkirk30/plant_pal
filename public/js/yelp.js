d13588071c306e7e0dc76707cd5de64053fae842
ThomBurt <93936296+ThomBurt@users.noreply.github.com>
2022-03-15 21:36:29 +0000

    style changes in store page

--- public/js/yelp.js
+++ public/js/yelp.js
@@ -28,21 +28,26 @@ function listAppend(data) {
       let businessName = data[i].name;
       let businessURL = data[i].url;
       const rating = data[i].rating;
+      const price = data [i].price;
 
       const li = document.createElement("li");
       li.textContent = "";
       let ratingImg = document.createElement("img");
       ratingImg.src = `/img/yelp-stars/${rating}.png`;
-      ratingImg.setAttribute("style", "width: 25%; float: right; ")
+      ratingImg.setAttribute("style", "width: 14%; float: right; ")
+
+      let priceEl = document.createElement("P");
+      priceEl.innerText = price;
 
       let aText = document.createElement("a");
       aText.innerText = businessName;
-      li.setAttribute("style", "list-style: none; padding: 5px; background-color: #FFFDD0; border-radius: 5px; color: #5b695b; font-size:20px; width:100%; cursor:pointer; margin-top:20px; margin-bottom:20px;");
+      li.setAttribute("style", "list-style: none; justify-content: space-between; padding: 5px; background-color: #FFFDD0; border-radius: 5px; color: #5b695b; font-size:20px; width:100%; cursor:pointer; margin-top:20px; margin-bottom:20px;");
       aText.href = businessURL;
 
 
       listEl.appendChild(li);
       li.appendChild(aText);
+      // li.appendChild(priceEl);
       li.appendChild(ratingImg);
     }
   };
